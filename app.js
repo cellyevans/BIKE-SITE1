f03ed91dc7d2a6391a43d4d0733e40eecd9d2d71
Jenny <onyenuchecelestina@gemail.com>
2023-04-12 12:28:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function change1() {
+    document.getElementById('motor').src="./image/red.png";
+}
+
+function change2() {
+    document.getElementById('motor').src="./image/black.png";
+}
+
+function change3() {
+    document.getElementById('motor').src="./image/blue.png";
+}
+
+function change4() {
+    document.getElementById('motor').src="./image/yellow.png";
+}
+
+function change5() {
+    document.getElementById('motor').src="./image/green.png";
+}
+
